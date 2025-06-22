c11a8eb4c5511b148bfda0ddb5ce27d7961d5997
Steve Collins <steve.collins.jc@gmail.com>
2025-06-22 11:57:04 -0400

    Added SignUp method

--- sample/BitzArt.Blazor.Auth.SampleApp/BitzArt.Blazor.Auth.SampleApp/Services/SampleAuthenticationService.cs
+++ sample/BitzArt.Blazor.Auth.SampleApp/BitzArt.Blazor.Auth.SampleApp/Services/SampleAuthenticationService.cs
@@ -3,7 +3,7 @@
 namespace BitzArt.Blazor.Auth.SampleApp.Services;
 
 public class SampleAuthenticationService(JwtService jwtService)
-    : AuthenticationService<SignInPayload>()
+    : AuthenticationService<SignInPayload, SignUpPayload>()
 {
     public override Task<AuthenticationResult> SignInAsync(SignInPayload signInPayload, CancellationToken cancellationToken = default)
     {
@@ -20,4 +20,16 @@ public class SampleAuthenticationService(JwtService jwtService)
 
         return Task.FromResult(authResult);
     }
+
+    public override Task<AuthenticationResult> SignUpAsync(SignUpPayload signUpPayload, CancellationToken cancellationToken = default)
+    {
+        var jwtPair = jwtService.BuildJwtPair();
+        var authResult = Success(jwtPair);
+
+        return Task.FromResult(authResult);
+    }
+}
+
+public class SignUpPayload
+{
 }
